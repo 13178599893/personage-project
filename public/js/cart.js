8fcd455281e3b8cf0d247dd986a83751d51252dc
luoyulin <lyl@lyl.com>
2019-07-17 22:04:50 +0800

    cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,11 +1,13 @@
-var div=document.getElementById("shop-cart");
+let div=document.getElementById("shop-cart");
+console.log(div)
 div.onclick=function(e){
     var btn=e.target;
+    
     if(btn.nodeName=="BUTTON"){
         var span=btn.parentNode.children[1];
-        console.log(span)
+        // console.log(span)
         var n=parseInt(span.innerHTML);
-        console.log(n)
+        // console.log(n)
         if(btn.innerHTML=="+"){
             n++;
         }else if(n>1){
@@ -15,28 +17,63 @@ div.onclick=function(e){
         // }
         span.innerHTML=n;
         var tr=btn.parentNode.parentNode;
-        console.log(tr)
+        // console.log(tr)
         var price=parseFloat(
         tr.children[3].children[0].innerHTML.slice(1));
         var subtotal=price*n;
+        // console.log(subtotal)
         tr.children[5].innerHTML=`¥${subtotal.toFixed(2)}`;
         var totalAll=document.getElementById("total");
-        console.log(totalAll);
-        // var prices=parseFloat(btn.parentNode.nextElementSibling.innerHTML.slice(1));
-        var prices=div.querySelectorAll("td.cart-six");
+        
+    }else{
+        return;
+        console.log(111)
+    }
+    var subtotals=document.getElementsByClassName("cart-six m-0 my_price");
+    console.log(subtotals)
+     var total=0;
+     let val = []; 
+     for(let item of subtotals) {
+        val.push(parseFloat(item.innerHTML.slice(1)));
+         console.log(val)
+     }
+     for(let item of val) {
+         total += item;
+     }
+      totalAll.innerHTML=`¥${total.toFixed(2)}`
+}
 
-        console.log(prices)
-        var total=0;
-        for(var c of prices){
-            total+=c.innerHTML.slice(1)
+
+
+//删除一行
+let father=document.getElementById("shopcartF2");
+// console.log(father)
+father.onclick=function(e){
+    var btnn=e.target;
+  
+    if(btnn.nodeName=="BUTTON"){
+        if(btnn.innerHTML=="x"){
+            var cart_items=document.getElementsByClassName("cart-items")
+            for(var cart of cart_items){
+             console.log(cart)
+             btnn.parentNode.parentNode.parentNode.parentNode.parentNode.remove();
         }
-        console.log(c.innerHTML.slice(1))
-        // console.log(total)
-        // var total=0;
-        // for(var p of prices){
-        // total+=p.children[0].innerHTML;
-        //   }
-        // // console.log(total)
-        // totalAll.innerHTML=`¥${total.toFixed(2)}`
+        }
     }
 }
+
+
+//删除全选
+var deleteAll=document.getElementById("deleteAll");
+console.log(deleteAll)
+deleteAll.addEventListener("click",function(){
+    var btnd=this;
+    console.log(1)
+    // var items=document.getElementsByClassName("cart-items");
+     this.parentNode.parentNode.parentNode.children[1].remove();
+    btnd.parentNode.parentNode.previousElementSibling.remove()
+    btnd.parentNode.parentNode.remove();
+    var empty=document.getElementById("empty");
+    empty.className="col-12 p-0"
+    // console.log(cart_items)
+})
--- public/js/header.js
+++ public/js/header.js
@@ -1,30 +1,63 @@
+//点击
+//找到所有的要触发事件的元素
+/*
+var as = document.querySelectorAll("[data-toggle='tab']");
+console.log(as);
+//找到所有要发生改变的元素
+let tabPaneArr = document.getElementsByClassName("tab-pane"); // [0,1,2,3,4,5]
+console.log(tabPaneArr);
+//遍历要触发事件
+for (let i = 0; i < as.length; i++) {
+  let tabThis = tabPaneArr[i];
+  console.log(tabThis);
+  //为每个as集合中的元素绑定事件处理函数
+  as[i].onclick= function() {
+   
+    if (tabThis.className == "tab-pane active show") {
+      tabThis.className = "tab-pane";
+      console.log(tabThis.className);
+    } else {
+        //每次添加显示的类名时先重置每一项的类名,防止多个显示
+        for (var item of tabPaneArr) {
+            item.className = "tab-pane";
+        }
+      tabThis.className += " active show";
+      console.log(tabThis.className);
+    }
+  };
+}
+  //导航栏的隐藏事件
+  
+  // window.onclick=function(e){
+  //   for (var item of tabPaneArr){
+  
+  //     if(e.target!==as){
+  //       item.style.display="none";
+  //     }
+  //   }
+  //   console.log(item.className)
+  // }
 
-//    for(var a of as){
-//        console.log(a)
-//        a.onclick=function(){
-//            var a=this;
-//            var divs=document.getElementsByClassName("tab-pane");
-//            console.log(divs)
-//            for(var div of divs){
+// var ul=document.getElementById("nav-nav");
+// var ass = document.querySelectorAll("[data-toggle='tab']");
+// let tabPaneArrs = document.getElementsByClassName("tab-pane");
+//   console.log(ul)
+//   console.log(as)
+ 
+//   window.onclick=function(e){
+//     var a=e.target;
+//     console.log(a)
+//     for(var items of ass){
+//       if(a!==items){
+//         console.log(items)
+//       }
+//     }
+//   }
 
-//                 // console.log(style)
-//                    div.getAttribute("class")
-//                    console.log(div.getAttribute("class"))
-//                 if(div.className=="tab-pane active show"){
-//                     console.log(1)
-//                     div.className="tab-pane";
-//                     console.log(2)
-//                 // }else if(div.className=="active"){
-//                 //         div.className="none";
-//                     // var active=document.querySelector("#tab-content>.active")
-//                     // console.log(active)
-//                     // active!==null&&(div.className="none");
-//                 }else {
-//                     div.className += " active show"
-//                 }
-//            }
-//        }
-//    }
+*/
+   
+
+//悬停
 //找到所有的要触发事件的元素
 var as = document.querySelectorAll("[data-toggle='tab']");
 console.log(as);
@@ -63,3 +96,4 @@ as[i].onmouseleave=function(){
     tabThis.className = "tab-pane";
 }
 }
+
